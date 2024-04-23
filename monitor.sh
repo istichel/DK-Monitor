a2b3aac85f45e410e08cfd3485e62ce93453ff57
Ivo Stichel <77754587+istichel@users.noreply.github.com>
2024-04-23 16:14:07 +0200

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -9,19 +9,14 @@ printf "15 3 * * * /usr/sbin/reboot &\n" >> mycron
 crontab mycron
 rm mycron
 
-##compatibel nur mit bullseyes
-xset s off
-xset s noblank
-xset -dpms
-
 ##start monitor
 touch startMonitor
 chmod 755 startMonitor
 printf "#!/bin/bash\n" >> startMonitor
 printf "export DISPLAY=:0;\n" >> startMonitor
-printf "xset s off" >> startMonitor
-printf "xset s noblank" >> startMonitor
-printf "xset -dpms" >> startMonitor
+printf "xset s off;\n" >> startMonitor
+printf "xset s noblank;\n" >> startMonitor
+printf "xset -dpms;\n" >> startMonitor
 printf "killall chromium-browser;\n" >> startMonitor
 printf "sleep 2;\n" >> startMonitor
 printf "chromium-browser -kiosk -private-window $2;\n" >> startMonitor
